d0e0f9565a1fb1952fb135cf78a9be9e610beafc
ShaikhZuber09~ <zs618272@gmail.com>
2023-09-15 20:57:11 +0530

    work on responsive

---
+++ assets/js/app.js
@@ -0,0 +1,6 @@
+
+const navigation=document.getElementById("navigation");
+const navToggle=document.getElementById("navToggle");
+
+
+navToggle.addEventListener("click",()=>{navigation.classList.toggle("active")})
